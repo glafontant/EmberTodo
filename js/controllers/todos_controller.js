4163fcb83ed414d40d4ae95e28959e66de257085
Gabe Lafontant <glafonta@gmail.com>
2014-02-16 17:39:51 -0500

    markingn model as complete or incomplete

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -20,3 +20,19 @@ Todos.TodosController = Ember.ArrayController.extend({
 	}
 });
 
+Todos.TodoController = Ember.ObjectController.extend({
+	isCompleted: function(key, value) {
+		var model = this.get('model');
+
+		if (value === undefined) {
+			// property being used as a getter
+			return model.get('isCompleted'); 
+		} else {
+			// propety being used as a setter
+			model.set('isCompleted', value);
+			model.save();
+			return value;
+		}
+	}.property('model.isCompleted')
+});
+
