9dedb436dad7281e1b0a4cf3094f3d2c4cf3511c
Gabe Lafontant <glafonta@gmail.com>
2014-02-17 14:18:49 -0500

    accepting edits

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -17,22 +17,15 @@ Todos.TodosController = Ember.ArrayController.extend({
 			// Save the new model
 			todo.save();
 		}
-	}
+	},
+	remaining: function() {
+		return this.filterBy('isCompleted', false).get('length');
+	}.property('@each.isCompleted'),
+
+	inflection: function() {
+		var remaining = this.get('remaining');
+		return remaining === 1 ? 'item' : 'items';
+	}.property('remaining') 
 });
 
-Todos.TodoController = Ember.ObjectController.extend({
-	isCompleted: function(key, value) {
-		var model = this.get('model');
-
-		if (value === undefined) {
-			// property being used as a getter
-			return model.get('isCompleted'); 
-		} else {
-			// propety being used as a setter
-			model.set('isCompleted', value);
-			model.save();
-			return value;
-		}
-	}.property('model.isCompleted')
-});
 
